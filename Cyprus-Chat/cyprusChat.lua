5b24d4ca046166967899e5882714d32f0ed51fda
RB98 <jamesrbenson03@gmail.com>
2018-01-31 21:01:33 -0500

    Added comments and code to contact CyprusCore
    Added comments for clarification of what certain things do and added code to contact CyprusCore and make sure it's online. I cannot test CyprusCore functionality as of yet without CyprusCore.

--- Cyprus-Chat/cyprusChat.lua
+++ Cyprus-Chat/cyprusChat.lua
@@ -2,12 +2,27 @@ chat = peripheral.wrap("right")
 speaker = peripheral.wrap("top")
 modem = peripheral.wrap("left")
 
+--Opens the modem to contact CyprusChat on channel 15672
+modem.open(15672)
+modem.transmit(15672, 15672, "CyprusChat calling CyprusCore")
 
+--Pulls the events in order to recieve CyprusCore's reply.
+event, senderChannel, replyChannel, modemMessage = os.pullEvent("modem_message")
+
+--Checks for the message from CyprusCore and errors if it is not recieved.
+if modemMessage = "CyprusCore is online" then
+  chat.say("CyprusChat is online and recieveing.")
+else
+  chat.say("Error, cannot connect to CyprusCore")
+end
+
+--Pulls the events for interacting with the player in from chat commands.
 while true do
   event, player, message = os.pullEvent("chat")
 
+--Start of command list.
 if message == "!nanoHeal" or message == "!NanoHeal" or message == "!Nanoheal" or message == "!nanoheal" then
   sleep(0.1)
-  chat.say("CyprusCore is not yet online, cannot connect to CyprusNano")
+
 end
 end
